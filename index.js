2c1deab7d12ff6478d6f4827ebcebdaa8e0731dd
Vincent Wochnik <v.wochnik@gmail.com>
2015-04-09 13:18:46 +0200

    update architecture

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*
+/**
  * assemble-plugin-index
  * https://github.com/vwochnik/assemble-plugin-index
  *
@@ -8,4 +8,14 @@
 
 'use strict';
 
-module.exports = require('./lib');
+var index = require('./lib');
+
+module.exports = function(assemble) {
+  // introduce renderable `index` template collection
+  assemble.create('index', 'indices', {isRenderable: true});
+
+  // forward call to library
+  return function() {
+    return index.apply(assemble, arguments);
+  };
+};
